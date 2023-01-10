6c469dc124a60aafe9e2f799be6ed150a4d48c6c
Oxygen <grandnorik@mail.ru>
2023-01-10 16:12:59 +0600

    Task1 done

--- Task1.py
+++ Task1.py
@@ -6,44 +6,53 @@
 
 import random
 
-
-def write_file(st):
+def CreateFile(st):
     with open('file.txt', 'w') as data:
         data.write(st)
+    print('file.txt created')
 
-
-def rnd():
-    return random.randint(0, 101)
-
-
-def create_mn(k):
-    lst = [rnd() for i in range(k+1)]
-    return lst
-
-
-def create_str(sp):
-    lst = sp[::-1]
-    wr = ''
-    if len(lst) < 1:
-        wr = 'x = 0'
+def CreateEq(k):
+    eq_str = ''
+    if k < 1:
+        eq_str = 'x = 0'
     else:
-        for i in range(len(lst)):
-            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst)-i-1}'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
-            elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
+        for k,v in equation.items():
+            if k == 1:
+                if v == 0:
+                    eq_str += f''
+                elif v == 1:
+                    eq_str += f'x + '
+                else:
+                    eq_str += f'{v}*x + '
+            elif k == 0:
+                if v == 0:
+                    eq_str += f''
+                else:
+                    eq_str += f'{v} + '
+            else:
+                if v == 0:
+                    eq_str += f''
+                elif v == 1:
+                    eq_str += f'x**{k} + '
+                else:
+                    eq_str += f'{v}*x**{k} + '
+        else:
+            if v == 0:
+                eq_str += f''
+            else:
+                eq_str = eq_str[:-3]
+        if eq_str.endswith('+ '):
+            eq_str = eq_str.rstrip(' + ')
+        eq_str += f' = 0'
+    return eq_str
 
+k = int(input('Введите максимальную степень: '))
+equation = {}
+for i in range(k, -1, -1):
+    equation[i] = random.randint(0,100)
+ 
+#print(equation)
 
-k = int(input("Введите натуральную степень k = "))
-koef = create_mn(k)
-write_file(create_str(koef))
+print(CreateEq(k))
 
+CreateFile(CreateEq(k))
